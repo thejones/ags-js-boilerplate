dbb1f35723819f0c6bb5d78b5d8b6c99072b95c2
Alex Jones <alexander.t.jones@gmail.com>
2013-01-31 21:47:37 -0700

    Many changes

--- app/run.js
+++ app/run.js
@@ -6,6 +6,7 @@
  * If you aren't planning on running your app on both the client and the server, you could easily move this
  * configuration into index.html (as a dojoConfig object) if it makes your life easier.
  */
+
 require({
 	// The base path for all packages and modules. If you don't provide this, baseUrl defaults to the directory
 	// that contains dojo.js. Since all packages are in the root, we just leave it blank. (If you change this, you
@@ -18,11 +19,6 @@ require({
 	// might be important to you if you need to relocate dependencies. TL;DR, register all your packages all the time:
 	// it will make your life easier.
 	packages: [
-		// If you are registering a package that has an identical name and location, you can just pass a string
-		// instead, and it will configure it using that string for both the "name" and "location" properties. Handy!
-		'dojo',
-		'dijit',
-		'dojox',
 		'jquery',
 
 		// For reference, this is what a more verbose package declaration looks like.
