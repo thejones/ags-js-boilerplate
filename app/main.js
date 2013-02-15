4f540800c333d541ddab92ba479b1592e431c0fd
Alex Jones <alexander.t.jones@gmail.com>
2013-02-15 09:00:15 -0700

    simple
    Simple commit. Removed define. Changed lable. fixed some whitespace.

--- app/main.js
+++ app/main.js
@@ -4,42 +4,39 @@
  * More information about everything described about the loader throughout this file can be found at
  * <http://dojotoolkit.org/reference-guide/loader/amd.html>.
  */
-define(['require'], function (require) {
-    /*dojo.require("esri.map");
-    dojo.require('esri.dijit.Popup');*/
-    var app = {};
-    require(["esri/map", "esri/layers/FeatureLayer", "js/Dialog", "js/AgsGeocode",
-        "js/ExampleWidget","dojo/dom","dojo/request","dojo/_base/array","dojo/parser", "dijit/layout/BorderContainer", "dijit/layout/ContentPane",
-        "dojo/domReady!"], function (Map, FeatureLayer, Dialog, AgsGeocode,ExampleWidget,dom,request,array, parser) {
 
-        // create the dijit layout widgets
-        parser.parse();
+var app = {};
+require(["esri/map", "esri/layers/FeatureLayer", "js/Dialog", "js/AgsGeocode",
+    "js/ExampleWidget","dojo/dom","dojo/request","dojo/_base/array","dojo/parser", "dijit/layout/BorderContainer", "dijit/layout/ContentPane",
+    "dojo/domReady!"], function (Map, FeatureLayer, Dialog, AgsGeocode,ExampleWidget,dom,request,array, parser) {
 
-        /*Dialog from dojo-bootstrap*/
-        app.dialog = new Dialog().placeAt(document.body);
-        app.dialog.startup();
-        app.dialog.show();
+    // create the dijit layout widgets
+    parser.parse();
 
-        //Map code and Geocode startup
-        app.map = new esri.Map("map", {
-          basemap: "topo",
-          center: [-122.41, 37.75],
-          zoom: 14
-        });
+    /*Dialog from dojo-bootstrap*/
+    app.dialog = new Dialog().placeAt(document.body);
+    app.dialog.startup();
+    app.dialog.show();
 
-       
-
-        dojo.connect(app.map, "onLoad", function () {
-            dojo.connect(dijit.byId("map"), "resize", app.map, map.resize);
-        });
-
-        /*Application "custom" geocode class*/
-        app.agsGeocode = new AgsGeocode({"map": app.map});
-        app.agsGeocode.createGeocode();
-
-        /*Application "custom" widget*/
-        var example = new ExampleWidget({}).placeAt(dom.byId("exampleWidgetContainer"));
-        
+    //Map code and Geocode startup
+    app.map = new esri.Map("map", {
+      basemap: "topo",
+      center: [-122.41, 37.75],
+      zoom: 14
     });
 
+   
+
+    dojo.connect(app.map, "onLoad", function () {
+        dojo.connect(dijit.byId("map"), "resize", app.map, map.resize);
+    });
+
+    /*Application "custom" geocode class*/
+    app.agsGeocode = new AgsGeocode({"map": app.map});
+    app.agsGeocode.createGeocode();
+
+    /*Application "custom" widget*/
+    var example = new ExampleWidget({}).placeAt(dom.byId("exampleWidgetContainer"));
+    
 });
+
